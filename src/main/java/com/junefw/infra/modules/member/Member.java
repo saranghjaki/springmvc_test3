e25d83a5ed20c2e2448715a4026028a566a46470
saranghjaki <3340513@naver.com>
2022-02-28 14:06:27 +0900

    s

---
+++ src/main/java/com/junefw/infra/modules/member/Member.java
@@ -0,0 +1,28 @@
+package com.junefw.infra.modules.member;
+
+public class Member {
+	private String pilmmSeq = "";
+	private String pilmmId = "";
+	private String pilmmName= "";
+	public String getPilmmSeq() {
+		return pilmmSeq;
+	}
+	public void setPilmmSeq(String pilmmSeq) {
+		this.pilmmSeq = pilmmSeq;
+	}
+	public String getPilmmId() {
+		return pilmmId;
+	}
+	public void setPilmmId(String pilmmId) {
+		this.pilmmId = pilmmId;
+	}
+	public String getPilmmName() {
+		return pilmmName;
+	}
+	public void setPilmmName(String pilmmName) {
+		this.pilmmName = pilmmName;
+	}
+	
+	
+
+}
---
+++ src/main/java/com/junefw/infra/modules/member/MemberService.java
@@ -0,0 +1,8 @@
+package com.junefw.infra.modules.member;
+
+import java.util.List;
+
+public interface MemberService {
+
+	public List<Member> selectList() throws Exception; 
+}
